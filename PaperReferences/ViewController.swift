1f9098fb24ac3ad096ba767d3c04fcbfb6a92364
Mudit Verma <mudit.verma2014@gmail.com>
2020-08-22 19:39:30 -0700

    Initial Commit

---
+++ PaperReferences/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PaperReferences
+//
+//  Created by Mudit Verma on 8/22/20.
+//  Copyright © 2020 Mudit Verma. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
